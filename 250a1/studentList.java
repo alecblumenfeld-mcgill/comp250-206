92188428bb6205df3f18dc0e40f5c049ace72847
alecblumenfeld-mcgill <alec.blumenfeld@mail.mcgill.ca>
2014-09-12 12:18:24 -0400

    added better itteration though list size

--- 250a1/studentList.java
+++ 250a1/studentList.java
@@ -131,31 +131,35 @@ class studentList {
 	//	secondList=new studentList("MATH240.txt", "MATH240 - Discrete Mathematics");
 	
 	// get the time before starting the intersections
-	long startTime = System.nanoTime();
-	int[] classSizes= {2,4,8,16,32,64,128,256,512,1000,4000,8000,16000,3200,64000,128000,256000,512000,1024000}; 
+	int[] classSizes= {2,4,8,16,32,64,128,256,512,1000,4000,8000,16000,32000,64000,128000,256000,512000,1024000}; 
 	int classPtr =0;
 	// repeat the process a certain number of times, to make more accurate average measurements.
 	for (int rep=0;rep<classSizes.length;rep++) {
-	    
+		long startTime = System.nanoTime();
+
 	    // This is how to generate lists of random IDs. 
 	    // For firstList, we generate 16000 IDs
 	    // For secondList, we generate 16000 IDs
-	    
+	    long listGenTimeBefore = System.nanoTime();
 	    firstList=new studentList(classSizes[classPtr] , "COMP250 - Introduction to Computer Science"); 
+	    long listGenTimeAfter = System.nanoTime();
+	    long listGenTime= (listGenTimeAfter -listGenTimeBefore)*2;
+	    
+	    //System.out.println(listGenTime);
+
 	    secondList=new studentList(classSizes[classPtr] , "MATH240 - Discrete Mathematics"); 
 	    
 	    // run the intersection method
-	    int intersection=studentList.intersectionSizeNestedLoops(firstList,secondList);
+	    int intersection=studentList.intersectionSizeBinarySearch(firstList,secondList);
 	   // System.out.println("The intersection size is: "+intersection);
-	    classPtr++;
 		long endTime = System.nanoTime();
-		System.out.println("Running time: "+ (endTime-startTime)/1000.0 + " nanoseconds:  Nested Loops"+"  list size is :"+classSizes[classPtr]);
+		System.out.println("Running time: "+ ((endTime-startTime)/1000.0) + " nanoseconds:  Binary Search"+"--  list size is :"+classSizes[classPtr]);
+	    classPtr++;
 
 
 	}
 	
 
-	 startTime = System.nanoTime();
 	// repeat the process a certain number of times, to make more accurate average measurements.
     }
 }
